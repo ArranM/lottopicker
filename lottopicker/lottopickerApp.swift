8b79f006a0ec3dbcfe3969e205aa55df7c61231c
ArranM <arran@web-site.co.uk>
2024-05-01 10:50:43 +0100

    Initial Commit

---
+++ lottopicker/lottopickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  lottopickerApp.swift
+//  lottopicker
+//
+//  Created by Arran Maclean on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct lottopickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
